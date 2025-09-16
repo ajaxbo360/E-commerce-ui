aa6c992ca5c1fceb161c85c4f963c812f0009532
brahim ajakak <brahim@dockflow.com>
2025-09-16 12:19:35 +0100

    producctlist and categorie

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { ProductsType, ProductType } from "@/types";
+import React, { FC } from "react";
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+  return <div>ProductCard</div>;
+};
+
+export default ProductCard;
